d7e42025000a8434d6a6b3786d855cdbae29aa6b
André <andrevictor50@gmail.com>
2020-10-14 09:13:53 -0300

    adding dimensions hook and divided header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,31 @@
 import { Container } from '../styles/components/Header';
 import { FiChevronDown } from 'react-icons/fi';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 export default function Header() {
+  const { width } = useWindowDimensions();
+
   return (
     <Container>
-      <div>
-        <span>Entregar em</span>
-        <main>
-          <h3>Av. Ceará, 1039</h3>
-          <FiChevronDown color="#ea1d2c" size={18} />
-        </main>
-      </div>
+      {
+        width < 960 ? (
+          <div className="mobile" >
+            <span>Entregar em</span>
+            <main>
+              <h3>Av. Ceará, 1039</h3>
+              <FiChevronDown color="#ea1d2c" size={18} />
+            </main>
+          </div>
+        ) : (
+          <div className="desktop" >
+            <span>Desktop</span>
+            <main>
+              <h3>Av. Ceará, 1039</h3>
+              <FiChevronDown color="#ea1d2c" size={18} />
+            </main>
+          </div>
+        )
+      }
     </Container>
   )
 }
