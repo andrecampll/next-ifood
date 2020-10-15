91da29a7df9ca439bd2418e0e80e040cce0bb45d
André <andrevictor50@gmail.com>
2020-10-15 12:26:02 -0300

    automatic render to categories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,141 +1,45 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Container } from '../styles/components/Categories';
+import api from '../services/api';
+
+interface ICategory {
+  id: number,
+  title: string,
+  image_url: string;
+}
 
 export default function Categories() {
+  const [categories, setCategories] = useState<ICategory[]>([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const response = await api.get('categories');
+
+      setCategories(response.data);
+    }
+
+    loadCategories();
+  }, []);
+
   return (
     <Container className="scroll-box">
       <div role="list" className="scroll-box__wrapper">
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/groc_icon_alegre.png"
-              alt="Mercado"
-            />
-          </figure>
-          <span>Mercado</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/Promo_cat.png"
-              alt="Promoções"
-            />
-          </figure>
-          <span>Promoções</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/20C1_VR_Test_AB.png"
-              alt="Vale Refeição"
-            />
-          </figure>
-          <span>Vale-Refeição</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-brasileira-v2.jpg"
-              alt="Brasileira"
-            />
-          </figure>
-          <span>Brasileira</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-saudavel-v2.jpg"
-              alt="Saudável"
-            />
-          </figure>
-          <span>Saudável</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-marmita.jpg"
-              alt="Marmita"
-            />
-          </figure>
-          <span>Marmita</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <Link href="categories/Lanches"  >
-              <img
-                src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-lanches-v2.jpg"
-                alt="Lanches"
-              />
-            </Link>   
-          </figure>
-          <span>Lanches</span>
-        </main>
-      
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-japonesa.jpg"
-              alt="Japonesa"
-            />
-          </figure>
-          <span>Japonesa</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-doces-e-bolos.jpg"
-              alt="Doces & Bolos"
-            />
-          </figure>
-          <span>Doces & Bolos</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-italiana.jpg"
-              alt="Italiana"
-            />
-          </figure>
-          <span>Italiana</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-carnes-v2.jpg"
-              alt="Carnes"
-            />
-          </figure>
-          <span>Carnes</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-pizza.jpg"
-              alt="Carnes"
-            />
-          </figure>
-          <span>Pizza</span>
-        </main>
-
-        <main role="listitem" >
-          <figure>
-            <img
-              src="https://static-images.ifood.com.br/image/upload/t_low/discoveries/19C1-chinesa.jpg"
-              alt="Chinesa"
-            />
-          </figure>
-          <span>Chinesa</span>
-        </main>
+        {
+          categories?.map(category => (
+            <main role="listitem" >
+              <figure>
+                <Link href={`categories/${category.title}`}  >
+                  <img
+                    src={category.image_url}
+                    alt={category.title}
+                  />
+                </Link>   
+              </figure>
+              <span>{category.title}</span>
+            </main>
+          ))
+        }
       </div>
     </Container>
   )
