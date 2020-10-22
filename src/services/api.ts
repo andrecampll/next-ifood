0c2a9e13bbb35081cc55264c95074931e34e1c2a
André <andrevictor50@gmail.com>
2020-10-22 14:00:25 -0300

    new api url for demo live

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://my-json-server.typicode.com/andrecampll/api-project',
 });
 
 export default api;
