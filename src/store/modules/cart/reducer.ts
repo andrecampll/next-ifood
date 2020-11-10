130ab6ddd867c1c3a61e57d3b2344d6d15b7b9b5
André <andrevictor50@gmail.com>
2020-11-10 01:52:53 -0300

    state to failed stock check foods

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -4,6 +4,7 @@ import { ICartState } from './types';
 
 const INITIAL_STATE: ICartState = {
   items: [],
+  failedStockCheck: [],
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
@@ -29,7 +30,7 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
       }
 
       case 'ADD_FOOD_TO_CART_FAILURE': {
-        console.log('failure:', action.payload);
+        draft.failedStockCheck.push(action.payload.foodId);
 
         break;
       }
--- src/store/modules/cart/types.ts
+++ src/store/modules/cart/types.ts
@@ -13,4 +13,5 @@ export interface ICartItem {
 
 export interface ICartState {
   items: ICartItem[];
+  failedStockCheck: string[];
 }
