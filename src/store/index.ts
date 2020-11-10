b79be05ea17f4d376a49e28ddb082f38d7608e81
André <andrevictor50@gmail.com>
2020-11-10 01:03:59 -0300

    adding devtools for redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer';
 
@@ -6,6 +7,6 @@ export interface IState {
   cart: ICartState;
 }
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
