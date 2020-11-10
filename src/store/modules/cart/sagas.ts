d72f73fd3c57163bec8766b9cb227b8c7641df32
André <andrevictor50@gmail.com>
2020-11-10 01:24:58 -0300

    getting quantity on saga

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,7 +1,19 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, takeLatest, select } from 'redux-saga/effects';
+import { IState } from '../..';
+import { addFoodToCart } from './actions';
 
-function checkFoodStock() {
-  console.log('adicionou');
+type CheckProductStockRequest = ReturnType<typeof addFoodToCart>;
+
+function* checkFoodStock({ payload }: CheckProductStockRequest) {
+  const { food } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find(item => item.food.id === food.id)?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantity);
 }
 
 export default all([takeLatest('ADD_FOOD_TO_CART', checkFoodStock)]);
