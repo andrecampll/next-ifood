e30f700476303e0a1309ba84a89e9317b3821569
André <andrevictor50@gmail.com>
2020-11-13 16:23:29 -0300

    redirectionating user to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Home from './lista-restaurantes';
 
-export default function index() {
+export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('lista-restaurantes');
+  }, [router]);
+
   return <Home />;
 }
